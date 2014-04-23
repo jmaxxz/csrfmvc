64ccb98e7dab885e78fbd847236d2906b749c204
jmaxxz <jmaxxz@users.noreply.github.com>
2014-04-22 19:05:54 -0500

    Fully working version

--- CsrfMvc/Controllers/BankAccountController.cs
+++ CsrfMvc/Controllers/BankAccountController.cs
@@ -13,14 +13,48 @@ namespace CsrfMvc.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Secure(double amount, string action)
+        public ActionResult Secure(double amount, string mode)
         {
-            if("deposit".Equals(action, StringComparison.InvariantCultureIgnoreCase)){
-                vault.AddToAccount(HttpContext.User.Identity.Name, amount);
+            if ("deposit".Equals(mode, StringComparison.InvariantCultureIgnoreCase))
+            {
+                vault.AddToAccount(HttpContext.User.Identity.Name, secure: amount);
             }
 
-            if("withdraw".Equals(action, StringComparison.InvariantCultureIgnoreCase)){
-                vault.AddToAccount(HttpContext.User.Identity.Name, -amount);
+            if ("withdraw".Equals(mode, StringComparison.InvariantCultureIgnoreCase))
+            {
+                vault.AddToAccount(HttpContext.User.Identity.Name, secure: -amount);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Insecure(double amount, string mode)
+        {
+            if ("deposit".Equals(mode, StringComparison.InvariantCultureIgnoreCase))
+            {
+                vault.AddToAccount(HttpContext.User.Identity.Name, insecure: amount);
+            }
+
+            if ("withdraw".Equals(mode, StringComparison.InvariantCultureIgnoreCase))
+            {
+                vault.AddToAccount(HttpContext.User.Identity.Name, insecure: -amount);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Nonstandard(double amount, string mode)
+        {
+            if ("deposit".Equals(mode, StringComparison.InvariantCultureIgnoreCase))
+            {
+                vault.AddToAccount(HttpContext.User.Identity.Name, nonstandard: amount);
+            }
+
+            if ("withdraw".Equals(mode, StringComparison.InvariantCultureIgnoreCase))
+            {
+                vault.AddToAccount(HttpContext.User.Identity.Name, nonstandard: -amount);
             }
 
             return RedirectToAction("Index", "Home");
--- CsrfMvc/Models/Vault.cs
+++ CsrfMvc/Models/Vault.cs
@@ -3,28 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Web;
+using System.Runtime.Serialization;
 
 namespace CsrfMvc.Models
 {
+    [DataContract]
+    public class UserBalance{
+        [DataMember]
+        public double Secure {get; set;}
+        [DataMember]
+        public double Insecure {get; set;}
+        [DataMember]
+        public double Nonstandard {get; set;}
+    }
     public class Vault
     {
         private static Lazy<Uri> redisCloudUrl = new Lazy<Uri>(() => new Uri(ConfigurationManager.AppSettings["REDISCLOUD_URL"].ToString()));
 
-        public void AddToAccount(string accountName, double amount)
+        public void AddToAccount(string accountName, double secure = 0, double insecure = 0, double nonstandard = 0)
         {
             using (RedisClient redis = new RedisClient(redisCloudUrl.Value))
             {
-                var currentValue = redis.Get<double>(accountName);
-                redis.Set<double>(accountName, currentValue+amount);
+                var currentValue = redis.Get<UserBalance>(accountName) ?? new UserBalance();
+                currentValue.Secure += secure;
+                currentValue.Insecure += insecure;
+                currentValue.Nonstandard += nonstandard;
+                redis.Set<UserBalance>(accountName, currentValue);
             }
         }
 
-        public double GetBalance(string accountName)
+        public UserBalance GetBalance(string accountName)
         {
             using (RedisClient redis = new RedisClient(redisCloudUrl.Value))
             {
-                return redis.Get<double>(accountName);
+                return redis.Get<UserBalance>(accountName) ?? new UserBalance();
             }
         }
     }
